16cbabc07b95d2d4256d435a318212ba68850083
Andre Rocha <andre.rocha@cern.ch>
2017-12-20 03:18:57 +0100

    cosmetic improvements

--- readable/src/App.js
+++ readable/src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Post from "./components/Post.js"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="menuContainer">
+                    <ul className="menu">
+                        <li className="menuItem" style={{'font-family': 'Pacifico', 'font-size':'40px'}}>Readable</li>
+                        <li className="menuItem">Fun Things</li>
+                        <li className="menuItem">Life Topics</li>
+                        <li className="menuItem">Philosophy</li>
+                        <li className="menuItem">Literature</li>
+                    </ul>
+                </div>
+
+                <div className="postList">
+                    <Post
+                        title="The best guapa in the world"
+                        author="Andre Rocha"
+                        body="The best guapa in the world is Paz Arroyo. Valencian by nature and dragon by right. She likes eating, fiesta, and gets angry when she cooks. If you keep away from her when she cooks you'll have a lovely time. She's just adorable"
+                        category="fiesta"
+                        timestamp="12/03/2018"
+                    />
+                    <Post
+                        title="The best guapa in the world"
+                        author="Andre Rocha"
+                        body="The best guapa in the world is Paz Arroyo. Valencian by nature and dragon by right. She likes eating, fiesta, and gets angry when she cooks. If you keep away from her when she cooks you'll have a lovely time. She's just adorable"
+                        category="fiesta"
+                        timestamp="12/03/2018"
+                    />
+                    <Post
+                        title="The best guapa in the world"
+                        author="Andre Rocha"
+                        body="The best guapa in the world is Paz Arroyo. Valencian by nature and dragon by right. She likes eating, fiesta, and gets angry when she cooks. If you keep away from her when she cooks you'll have a lovely time. She's just adorable"
+                        category="fiesta"
+                        timestamp="12/03/2018"
+                    />
+                    <Post
+                        title="The best guapa in the world"
+                        author="Andre Rocha"
+                        body="The best guapa in the world is Paz Arroyo. Valencian by nature and dragon by right. She likes eating, fiesta, and gets angry when she cooks. If you keep away from her when she cooks you'll have a lovely time. She's just adorable"
+                        category="fiesta"
+                        timestamp="12/03/2018"
+                    />
+                </div>
+
+            </div>
+        )
+
+    }
 }
 
 export default App;
--- readable/src/components/Post.js
+++ readable/src/components/Post.js
@@ -1,3 +1,20 @@
 /**
  * Created by apaivaer on 19/12/2017.
  */
+import React, { Component } from 'react';
+
+class Post extends Component {
+
+    render() {
+        return(
+            <div className="postDiv">
+                <h1 className="postTitle">{`${this.props.title}`}</h1>
+                <h2 className="postAuthor">{`by ${this.props.author}`}</h2>
+                <p className="postContent">{`${this.props.body}`}</p>
+                <h3 className="postTimeStampAndCategory">{`Posted in ${this.props.category} on ${this.props.timestamp}`}</h3>
+            </div>
+        )
+    }
+}
+
+export default Post
